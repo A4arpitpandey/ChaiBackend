63c268eabda522a70c7c31b1568369b02c3afc82
A4arpitpandey <arpitpandey1100@gmail.com>
2025-01-31 15:25:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twiter", (req, res) => {
+  res.send("arpit_pandey");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login on app</h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Please learn more my friend</h2>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
